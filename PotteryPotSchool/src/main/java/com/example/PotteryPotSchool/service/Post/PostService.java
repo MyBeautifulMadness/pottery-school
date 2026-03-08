fe3822859cc2569fe82d37898ec83b168802c64d
Mikhail Nosov <misha.d.nosov@gmail.com>
2026-03-09 00:38:44 +0700

    unit-тесты для удаления поста

--- PotteryPotSchool/src/main/java/com/example/PotteryPotSchool/service/Post/PostService.java
+++ PotteryPotSchool/src/main/java/com/example/PotteryPotSchool/service/Post/PostService.java
@@ -3,6 +3,9 @@ package com.example.PotteryPotSchool.service.Post;
 import com.example.PotteryPotSchool.dto.Posts.PostCreateRequest;
 import com.example.PotteryPotSchool.dto.Posts.PostDetails;
 
+import java.util.UUID;
+
 public interface PostService {
     PostDetails createPost(PostCreateRequest request);
+    void delete(UUID postId);
 }
