847de4c39f8d1642ac04556953a000957e20f630
Mikhail Nosov <misha.d.nosov@gmail.com>
2026-03-10 18:08:08 +0700

    unit-тесты для просмотра комментариев по postId

--- PotteryPotSchool/src/main/java/com/example/PotteryPotSchool/repository/CommentRepository.java
+++ PotteryPotSchool/src/main/java/com/example/PotteryPotSchool/repository/CommentRepository.java
@@ -3,7 +3,9 @@ package com.example.PotteryPotSchool.repository;
 import com.example.PotteryPotSchool.entity.Comments.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, UUID> {
+    List<CommentEntity> findAllByPostId(UUID postId);
 }
