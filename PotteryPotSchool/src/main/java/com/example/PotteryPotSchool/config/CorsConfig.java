3d5a0558976e0bf977cdfa97cec06e516a7bc41e
Konstanton Alimov <kostya.alimov.96@mail.ru>
2026-04-14 15:10:40 +0700

    cors haha 2

--- PotteryPotSchool/src/main/java/com/example/PotteryPotSchool/config/CorsConfig.java
+++ PotteryPotSchool/src/main/java/com/example/PotteryPotSchool/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5173")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5173", "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
